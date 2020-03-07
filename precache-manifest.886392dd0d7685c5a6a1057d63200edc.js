eff6857f23219a17edd7c23a25eef13053e8460c
priya4110 <priyatoshpatra@gmail.com>
2020-03-07 14:44:40 +0530

    Updates

---
+++ precache-manifest.886392dd0d7685c5a6a1057d63200edc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613f9db171a0b1558de19ce6ec9ab9b2",
+    "url": "/githubdemo/index.html"
+  },
+  {
+    "revision": "ef5388e611ace0c1f1c4",
+    "url": "/githubdemo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2718291e7466a9cd9689",
+    "url": "/githubdemo/static/js/2.19f65463.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/githubdemo/static/js/2.19f65463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5388e611ace0c1f1c4",
+    "url": "/githubdemo/static/js/main.ae4b6803.chunk.js"
+  },
+  {
+    "revision": "c0d217c69471fbedfd82",
+    "url": "/githubdemo/static/js/runtime-main.5b664fd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubdemo/static/media/logo.5d5d9eef.svg"
+  }
+]);
